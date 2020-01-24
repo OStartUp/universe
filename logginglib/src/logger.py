fb1cfa212f3217ba65f83f152a40f7001ef2c7b8
mcaro <marcelo_carovargas@mcafee.com>
2020-01-24 11:29:46 -0800

    Adding BUILD as dependency

--- logginglib/src/logger.py
+++ logginglib/src/logger.py
@@ -3,6 +3,9 @@
 # from config import getConfig
 from config.src import getConfig
 
+def testeable(param):
+    return param
+
 def logg(msg):
     config = getConfig() 
     print(f"{config} - {msg}")
