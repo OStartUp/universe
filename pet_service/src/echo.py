d45d8b35cf6ae09b2dcdc9fa68f8a390fef17ad6
mcaro <marcelo_carovargas@mcafee.com>
2020-01-30 14:15:53 -0800

    Inc

--- pet_service/src/echo.py
+++ pet_service/src/echo.py
@@ -78,7 +78,7 @@ def echo(path):
         'form' : extract(request.form),
         'json' : request.json,
         'cookies' : extract(request.cookies),
-        'service': "pet"
+        'service': "PETTT"
     }
 
     if config[VERBOSE]:
